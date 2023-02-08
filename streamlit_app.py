aa61e5125c7aacd4a4c2d20f1b7477b54f200436
Prajaktaak <124554626+Prajaktaak@users.noreply.github.com>
2023-02-08 11:17:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,6 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-
-
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
@@ -39,7 +37,7 @@ try:
          streamlit.dataframe(fruityvice_normalized)
         
  except URLError as e:
-     streamlit.error()
+    streamlit.error()
 
 # don't run anything past here while we troubleshoot
 streamlit.stop()
